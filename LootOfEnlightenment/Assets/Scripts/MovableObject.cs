43b890f160547a0dd6023b5b936ea4d3a7f63b7d
Unknown <dmajonica@aol.com>
2019-09-11 13:36:53 +0200

    adds simple character movement

---
+++ LootOfEnlightenment/Assets/Scripts/MovableObject.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class MovableObject : MonoBehaviour
+{
+    protected virtual void Move(Vector3 direction, float speed)
+    {
+        transform.position += direction.normalized * speed;
+    }
+}
---
+++ LootOfEnlightenment/Assets/Scripts/Player.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Player : MovableObject
+{
+    public float MovementSpeed = 0.1f;
+
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            Move(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")), MovementSpeed);
+        }
+    }
+}
