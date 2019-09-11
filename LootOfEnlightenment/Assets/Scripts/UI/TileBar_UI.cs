8a53e9f4d75fe5bdf26b70bc3d01b1f09c228e9f
Unknown <dmajonica@aol.com>
2019-09-11 21:04:48 +0200

    ui implementation 1

---
+++ LootOfEnlightenment/Assets/Scripts/UI/TileBar_UI.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileBar_UI : MonoBehaviour
+{
+    public GameObject Tile;
+    public int MaxAmount;
+
+}
