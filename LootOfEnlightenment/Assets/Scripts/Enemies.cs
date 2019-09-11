d95990750582239819b3458dd8a9d7160b82f65f
Nick Wang <nickwang1.29.7@gmail.com>
2019-09-11 14:27:47 +0200

    Emeny detection and movement

---
+++ LootOfEnlightenment/Assets/Scripts/Enemies.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemies : MovableObject
+{
+    private GameObject _playerCharacter;
+    public float DetectionRange;
+    public float Speed;
+    private void Start()
+    {
+        _playerCharacter = GameObject.FindWithTag("Player");
+    }
+    private void Update()
+    {
+        Vector3 playerInDirection = _playerCharacter.transform.position - transform.position;
+        if(playerInDirection.magnitude < DetectionRange)
+        {
+            Move(playerInDirection.normalized, Speed);
+        }
+        Debug.Log((_playerCharacter.transform.position - transform.position).magnitude);
+    }
+}
