031afd10a939ef2b4640ce4cf2016929b0387510
Nick Wang <nickwang1.29.7@gmail.com>
2019-09-11 17:04:09 +0200

    Start on PickUps

---
+++ LootOfEnlightenment/Assets/Scripts/PickUp.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public string Type;
+
+    private float _bonusSpeed = 1.5f;
+    private int _bonusHeal = 5;
+    private int _bonusAmmo = 10;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")   //health, ammo, experience, 
+        {
+            switch(Type)
+            {
+                case "Heal":
+                    //collision.gameObject.GetComponent<Player>().
+                    break;
+                case "Speed":
+                    collision.gameObject.GetComponent<Player>().MovementSpeed *= 1.5f;
+                    break;
+                case "Ammo":
+                    break;
+                case "Exp":
+                    break;
+                default:
+                    break;
+            }
+            Destroy(gameObject);
+        }
+    }
+}
