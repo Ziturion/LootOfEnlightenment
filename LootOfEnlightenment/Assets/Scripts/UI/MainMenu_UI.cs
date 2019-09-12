fdadc85e28671917d1f51827b406ccd3de9ec0d4
Unknown <dmajonica@aol.com>
2019-09-12 12:23:38 +0200

    adds Main menu

---
+++ LootOfEnlightenment/Assets/Scripts/UI/MainMenu_UI.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu_UI : MonoBehaviour
+{
+    public GameObject StartPanel;
+    public GameObject CreditsPanel;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void BackToStartPanel()
+    {
+        CreditsPanel.SetActive(false);
+
+        StartPanel.SetActive(true);
+    }
+
+    public void ToPanel(string panelName)
+    {
+        StartPanel.SetActive(false);
+        switch (panelName)
+        {
+            case "Credits":
+                CreditsPanel.SetActive(true);
+                break;
+            default:
+                break;
+        }
+    }
+}
